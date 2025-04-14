959d1bfe25b14d2c99313213f5d7b493e5a2bfcc
imad kasse <kasseimad81@gmail.com>
2025-04-14 18:07:10 +0100

    Add Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale } from "next-intl/server";
-import { Cairo } from "next/font/google";
+import { Cairo ,Geist  } from "next/font/google";
 import "./globals.css";
 
 const cairo = Cairo({
   subsets: ["arabic"],
 });
+const geist = Geist({
+  subsets: ['latin'],
+})
 export default async function RootLayout({
   children,
 }: {
@@ -13,9 +16,10 @@ export default async function RootLayout({
 }) {
   const locale = await getLocale();
   const dir = locale === "ar" ? "rtl" : "ltr";
+  const font = locale === "ar" ? cairo.className : geist.className;
 
   return (
-    <html lang={locale} dir={dir} className={cairo.className}>
+    <html lang={locale} dir={dir} className={font}>
       <body>
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
