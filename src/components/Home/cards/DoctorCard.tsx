fcd054bab1fe334b662e5c238ef8d6ddcf4ec4b4
imad kasse <kasseimad81@gmail.com>
2025-05-04 08:08:03 +0100

    add Footer and /services

--- src/components/Home/cards/DoctorCard.tsx
+++ src/components/Home/cards/DoctorCard.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Phone } from "lucide-react";
-import Image from "next/image";
 
-interface DoctorCardProps {
-  name: string;
-  specialty: string;
-  description: string;
-  img: string;
-}
 
-const DoctorCard = ({ name, specialty, description, img }: DoctorCardProps) => {
+// interface DoctorCardProps {
+//   name: string;
+//   specialty: string;
+//   description: string;
+//   img: string;
+// }
+
+const DoctorCard = () => {
   return (
     <div></div>
   );
