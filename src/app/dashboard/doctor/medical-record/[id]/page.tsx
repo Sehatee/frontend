e4cddee80a67169133a05c955fb1221cacc0aa96
imad kasse <kasseimad81@gmail.com>
2025-05-28 18:55:33 +0100

    fix error in /dashboard/doctor/medical-record/[id]

--- src/app/dashboard/doctor/medical-record/[id]/page.tsx
+++ src/app/dashboard/doctor/medical-record/[id]/page.tsx
@@ -1,26 +1,18 @@
 import MedicalRecord from "@/components/medicalRecord/MedicalRecord";
 import { getOneMedicalRecordByDoctor } from "@/lib/api/medicalRecord";
 import { MedicalRecordDoctor } from "@/types/MedicalRecord";
-import { User } from "@/types/User";
+
 import { cookies } from "next/headers";
 import React from "react";
 
 interface Props {
   params: Promise<{ id: string }>;
 }
-export async function generateStaticParams() {
-  const res = await fetch(
-    `${process.env.API_URL}/doctors`
-  );
-  const data = await res.json();
 
-  return data.doctors.map((doctor: User) => ({
-    doctorId: doctor._id,
-  }));
-}
 const page = async ({ params }: Props) => {
   const doctorId = (await params).id;
-  const token = (await cookies()).get("token")?.value;
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("token")?.value;
   const medicalRecordByDoctor: MedicalRecordDoctor =
     await getOneMedicalRecordByDoctor(token || "", doctorId);
   return (
