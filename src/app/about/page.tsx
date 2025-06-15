99f21d8a63212f798b9fa0a2355513f33e63bf62
mdYACINE <abderrahman.yacine@gmail.com>
2025-06-15 21:36:31 +0100

    edit aboutPage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const page = () => {
   
   return (
-    <div className="container mx-auto ">
+    <>
       <About />
-    </div>
+    </>
   );
 };
 
--- src/components/about/AboutCard.tsx
+++ src/components/about/AboutCard.tsx
@@ -6,24 +6,22 @@ const AboutCard = ({
   title,
   des,
 }: {
-  icon: React.ElementType; // Expecting a component
+  icon: React.ElementType;
   title: string;
   des: string;
 }) => {
-    
   return (
-    <div className="p-4 flex flex-col items-center gap-2 border-2 rounded shadow-md">
-      <div>
-        <Icon
-          color="#0B62DE"
-          className="p-2 bg-primary rounded-full"
-          size={60}
-        />
+    <div className="bg-white mx-auto lg:min-w-[420px] s:min-w-[350px] shadow-md rounded-2xl p-6 text-center flex flex-col items-center gap-4 transition-all hover:shadow-lg">
+      {/* دائرة الأيقونة */}
+      <div className="bg-[#eef4ff] p-4 rounded-full flex items-center justify-center">
+        <Icon size={32} className="text-main" />
       </div>
-      <div className="flex items-center mb-2">
-        <h3 className="text-lg font-bold">{title}</h3>
-      </div>
-      <p className="text-sm text-gray-700 w-80 text-center">{des}</p>
+
+      {/* العنوان */}
+      <h3 className="text-xl font-bold text-main">{title}</h3>
+
+      {/* الوصف */}
+      <p className="text-gray-600 text-sm max-w-xs">{des}</p>
     </div>
   );
 };
